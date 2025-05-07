8308456c41e2abecd0fc6455e025448c23f62e7a
braintop <braintop7@gmail.com>
2025-05-07 10:14:38 +0300

    add

--- 04setInterval/03setInterval.js
+++ 04setInterval/03setInterval.js
@@ -1,14 +1,20 @@
 /*
- שפר את הפונקציה 
+ שפר את הפונקציה 
 f 
 כך שתציג מספר שעולה ב 1 
 כל שניה 
 */
 
-function f(){
-    setInterval(()=>{
-        console.log("hello world")
-    } , 1000)
+function f() {
+    let counter = 0
+    setInterval(() => {
+        counter++
+        document.getElementById("timer").innerText = counter;
+        if (counter > 10) {
+            document.getElementById("timer").style.color = "blue"
+        }
+        console.log(counter)
+    }, 1000)
 }
 
 f()
