d8ed70762d255ac4212d98ee68620a4c92762411
braintop <braintop7@gmail.com>
2025-05-14 11:43:48 +0300

    add

--- 12-promise/exe1.js
+++ 12-promise/exe1.js
@@ -1,24 +1,27 @@
 function generatePrimeNumberAfterDelayAsync(min, max) {
     let p = new Promise((resolve, reject) => {
-        let number = min + Math.floor(Math.random() * (max - min))// min<number<max
-        let answer = true;
-        for (let i = 2; i <= number / 2; i++) {
-            if (number % i == 0) {
-                answer = false;
+        setTimeout(() => {
+            let number = min + Math.floor(Math.random() * (max - min))// min<number<max
+            let answer = true;
+            for (let i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    answer = false;
+                }
             }
-        }
-        if (answer == true) {
-            resolve("success : " + number + " is prime")
-        }
-        else {
-            reject("error : " + number + " is not prime")
-        }
+            if (answer == true) {
+                resolve("success : " + number + " is prime")
+            }
+            else {
+                reject("error : " + number + " is not prime")
+            }
+
+        }, 1000);
     })
     return p;
 }
 generatePrimeNumberAfterDelayAsync(10, 16)
     .then((data) => { console.log(data) })
-    .catch( (err) => { console.log(err)  })
+    .catch((err) => { console.log(err) })
 
 // let solution = generatePrimeNumberAfterDelayAsync(10, 16);
 // solution
